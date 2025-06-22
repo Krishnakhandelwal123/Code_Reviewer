f7d828ecd00034f932cb0888508ce3eb7ba3700d
Krishnakhandelwal123 <141343323+Krishnakhandelwal123@users.noreply.github.com>
2025-06-22 22:20:12 +0530

    project completed

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV === "production") {
   const frontendPath = path.join(__dirname, '..', 'frontend', 'dist');
   console.log('✅ Serving frontend from:', frontendPath);
 
-  app.use(express.static(frontendPath));
+  app.use(express.static(path.join(__dirname, '..', 'frontend', 'dist')));
 
   app.get('*', (req, res) => {
     res.sendFile(path.join(frontendPath, 'index.html'));
